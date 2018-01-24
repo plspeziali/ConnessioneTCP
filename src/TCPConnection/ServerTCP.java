2f78549a0a2e31d85dce2cbf108a9f9734952e1e
plspeziali <speziali99@gmail.com>
2018-01-24 18:49:46 +0100

    Added new classes

--- src/TCPConnection/ServerTCP.java
+++ src/TCPConnection/ServerTCP.java
@@ -1,3 +1,5 @@
+package TCPConnection;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -8,7 +10,7 @@
  *
  * @author paolo
  */
-public class start {
+public class ServerTCP {
 
     /**
      * @param args the command line arguments
